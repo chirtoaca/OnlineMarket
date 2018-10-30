8e85f9166381ec24f76dd78f6e840e94d86d84b6
Gheorghe <gheorghe.chirt@yahoo.com>
2018-10-30 20:57:16 +0200

    nomoddify

--- OnlineMarket.DataAccess/Repository/IRepository.cs
+++ OnlineMarket.DataAccess/Repository/IRepository.cs
@@ -14,6 +14,6 @@ namespace OnlineMarket.DataModels.Repository
         void Delete<T>(T entity) where T : class;
         //void Delete<T>(Expression<Func<T, bool>> where) where T : class;
         //IEnumerable<T> GetMany<T>(Expression<Func<T, bool>> where) where T : class;
-        //T Get<T>(Expression<Func<T, bool>> where) where T : class;    }
+        // T Get<T>(Expression<Func<T, bool>> where) where T : class;    
     }
 }
