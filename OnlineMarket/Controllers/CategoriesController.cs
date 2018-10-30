6625f8812666707d75929111b8cec3d056bb587f
Gheorghe <gheorghe.chirt@yahoo.com>
2018-10-30 20:58:44 +0200

    Created UpdateCategory, DeleteCategory and AddCategory ActionResult

--- OnlineMarket/Controllers/CategoriesController.cs
+++ OnlineMarket/Controllers/CategoriesController.cs
@@ -2,8 +2,10 @@
 using OnlineMarket.DataModels.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Entity.Validation;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,17 +19,53 @@ namespace OnlineMarket.Controllers
         {
             _categoryService = categoryService;
         }
-        // GET: Categories
+
+
         public ActionResult Index()
         {
             var categories = _categoryService.GetCategories();
             return View(categories);
         }
-
+        [HttpGet]
+        public ActionResult AddCategory ()
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult AddCategory(Category category)
         {
                  _categoryService.AddCategory(category);
                 return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteCategory(int Id)
+        {
+            Category cat = _categoryService.GetCategory(Id);
+            _categoryService.DeleteCategory(cat);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCategory(int Id = 0)
+        {
+            Category catUpdate = _categoryService.GetCategory(Id);
+
+            if (catUpdate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(catUpdate);
+        }
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _categoryService.UpdateCategory(category);
+                return RedirectToAction("Index");
+
+            }
+            return View(category);
         }
     }
 }
