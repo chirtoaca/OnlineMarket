7f5a33d01b580356bbddd8a5f5921582d09f037f
Gheorghe <gheorghe.chirt@yahoo.com>
2018-10-30 20:59:30 +0200

    Created Index, AddProduct, UpdateProduct, DeleteProduct, and DetailsProduct ActionResult

--- OnlineMarket/Controllers/ProductsController.cs
+++ OnlineMarket/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿
 using OnlineMarket.Core.Interfaces;
+using OnlineMarket.Core.Services;
+using OnlineMarket.DataModels.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,20 +13,86 @@ namespace OnlineMarket.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categorySerivice;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categorySerivice = categoryService;
+
         }
-
-
-
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(string option, string search)
         {
             var products = _productService.GetProducts();
-
             return View(products);
         }
+
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            ViewBag.Categories = _categorySerivice.GetCategories();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product product)
+        {
+
+            _productService.AddProduct(product);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteProduct(int id)
+        {
+            var result = _productService.GetProduct(id);
+            if (result != null)
+            {
+                _productService.DeleteProduct(result);
+                return RedirectToAction("Index");
+            }
+            return HttpNotFound();
+        }
+
+        public ActionResult UpdateProduct(int Id = 0)
+        {
+            ViewBag.Categories = _categorySerivice.GetCategories();
+            Product prodUpdate = _productService.GetProduct(Id);
+
+            if (prodUpdate == null)
+            {
+                HttpNotFound();
+            }
+            return View(prodUpdate);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.UpdateProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+
+        public ActionResult DetailsProduct(int Id)
+        {
+            Product prodDetails = _productService.GetProduct(Id);
+            var productDetails = _productService.GetProduct(Id);
+            if (prodDetails == null)
+            {
+                return HttpNotFound();
+            }
+            else
+                return View(productDetails);
+
+        }
+
+        
+
+        
     }
 }
