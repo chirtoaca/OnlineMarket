61bae8dc696fc235ca41a3fe740b0ba973cb4847
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-07 14:03:25 +0200

    Created new controller "NavigationController" and Menu method for CategoryMenu

---
+++ OnlineMarket/Controllers/NavigationController.cs
@@ -0,0 +1,27 @@
+﻿using OnlineMarket.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineMarket.Controllers
+{
+    public class NavigationController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public NavigationController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public PartialViewResult Menu(string category)
+        {
+            ViewBag.SelectedCategory = category;
+            var categories = _productService.GetProducts().Select(x => x.Category.Name);
+
+            return PartialView(categories);
+        }
+    }
+}
