de3965beeb931fdfe3e6688cb47ad892f5cc95e6
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-07 14:04:01 +0200

    Created new ViewModel with parametre for current categorory

---
+++ OnlineMarket/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using OnlineMarket.DataModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public List<Product> Products { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
