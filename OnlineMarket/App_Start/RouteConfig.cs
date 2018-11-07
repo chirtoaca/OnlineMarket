77fe5ae0bd22a0a5d4e2b63581c36a27efcddf62
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-07 14:01:59 +0200

    Added route for category select

--- OnlineMarket/App_Start/RouteConfig.cs
+++ OnlineMarket/App_Start/RouteConfig.cs
@@ -18,6 +18,22 @@ namespace OnlineMarket
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(null,
+            "",
+            new
+            {
+                controller = "Product",
+                action = "List",
+                category = (string)null,
+                page = 1
+            }
+                );
+
+            routes.MapRoute(null,
+             "{category}",
+            new { controller = "Product", action = "List", page = 1 }
+            );
         }
     }
 }
