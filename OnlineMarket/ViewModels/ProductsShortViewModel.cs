10222aab989e6f573a09fd1c41fb45b2213e1214
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-07 09:41:21 +0200

    Create ProductShortVIewModel

---
+++ OnlineMarket/ViewModels/ProductsShortViewModel.cs
@@ -0,0 +1,25 @@
+﻿using OnlineMarket.DataModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.ViewModels
+{
+    public class ProductsShortViewModel
+    {
+        
+        public int Id { get; set; }
+
+      
+        public string Name { get; set; }
+
+        public virtual Category Category { get; set; }
+
+       
+        public int CategoryId { get; set; }
+
+        public int Price { get; set; }
+    }
+}
