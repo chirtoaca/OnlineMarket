899cf79bf228136ffc04e918ae5fcfa6025f9235
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-15 12:48:26 +0200

    Implement Interface ICustomerService and define methods in Service

---
+++ OnlineMarket.Core/Interfaces/ICustomerService.cs
@@ -0,0 +1,20 @@
+﻿using OnlineMarket.DataModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Core.Interfaces
+{
+    public interface ICustomerService
+    {
+        List<Customer> GetCustomers();
+
+        Customer AddCustomer(Customer customer);
+        void RemoveCustomer(Customer customer);
+        void EditCustomer(Customer customer);
+
+        Customer GetCustomer(int id);
+    }
+}
---
+++ OnlineMarket.Core/Services/CustomerService.cs
@@ -0,0 +1,49 @@
+﻿using OnlineMarket.Core.Interfaces;
+using OnlineMarket.DataModels.Models;
+using OnlineMarket.DataModels.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Core.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IRepository _repository;
+
+        public CustomerService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Customer AddCustomer(Customer customer)
+        {
+            var addedCustomer = _repository.Add(customer);
+            return addedCustomer;
+        }
+
+        public void EditCustomer(Customer customer)
+        {
+            _repository.Update(customer);
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            var result = _repository.GetAll<Customer>().FirstOrDefault(o => o.Id == id);
+            return result;
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            var result = _repository.GetAll<Customer>().ToList();
+            return result;
+        }
+
+        public void RemoveCustomer(Customer customer)
+        {
+            _repository.Delete(customer);
+        }
+    }
+}
