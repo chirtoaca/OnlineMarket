6bcdeb69209d6ea4f596bc7c00d6ce6c45cf7ca1
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-15 12:43:30 +0200

    Added binder for sessionKey Cart

--- OnlineMarket/Global.asax.cs
+++ OnlineMarket/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineMarket.Entities;
+using OnlineMarket.Infrastructure.Binders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +18,7 @@ namespace OnlineMarket
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ModelBinders.Binders.Add(typeof(ShoppingCartService), new CartModelBinder());
         }
     }
 }
