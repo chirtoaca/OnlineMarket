4e76082b6edcda18dc0c349e6d6d2933218bcd58
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-15 12:51:36 +0200

    Created entities for Cart where define cart property

---
+++ OnlineMarket/Entities/CartLineItem.cs
@@ -0,0 +1,10 @@
+﻿using OnlineMarket.DataModels.Models;
+
+namespace OnlineMarket.Entities
+{
+    public class CartLineItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
