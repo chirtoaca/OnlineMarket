bbed1fc427c7bae3a6240707bb7d1e2e77e99fb7
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-15 12:49:29 +0200

    Implement IOrder interface and defined method in Service

---
+++ OnlineMarket.Core/Interfaces/IOrderService.cs
@@ -0,0 +1,20 @@
+﻿using OnlineMarket.DataModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Core.Interfaces
+{
+    public interface IOrderService
+    {
+        List<Order> GetOrders();
+
+        Order AddOrder(Order order);
+        void RemoveOrder(Order order);
+        void EditOrder(Order order);
+
+        Order GetOrder(int id);
+    }
+}
---
+++ OnlineMarket.Core/Services/OrderProductsService.cs
@@ -0,0 +1,47 @@
+﻿using OnlineMarket.Core.Interfaces;
+using OnlineMarket.DataModels.Models;
+using OnlineMarket.DataModels.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Core.Services
+{
+    public class OrderProductsService : IOrderProductService
+    {
+        private readonly IRepository _repository;
+
+        public OrderProductsService(IRepository repository)
+        {
+            _repository = repository;
+        }
+        public void AddOrderProducts(OrderProduct orderProducts)
+        {
+            _repository.Add(orderProducts);
+        }
+
+        public void EditOrderProducts(OrderProduct orderProducts)
+        {
+            _repository.Update(orderProducts);
+        }
+
+        public OrderProduct GetOrderProducts(int id)
+        {
+            var result = _repository.GetAll<OrderProduct>().FirstOrDefault(o => o.OrderId == id);
+            return result;
+        }
+
+        public List<OrderProduct> OrdersProducts()
+        {
+            var result = _repository.GetAll<OrderProduct>().ToList();
+            return result;
+        }
+
+        public void RemoveOrderProducts(OrderProduct orderProducts)
+        {
+            _repository.Delete(orderProducts);
+        }
+    }
+}
