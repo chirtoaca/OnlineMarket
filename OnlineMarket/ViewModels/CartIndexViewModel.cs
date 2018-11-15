4b45ae60d46acd6057474ead8f2d79b30a7059e6
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-15 12:52:42 +0200

    Created ViewModel for cart.

---
+++ OnlineMarket/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using OnlineMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.ViewModels
+{
+    public class CartIndexViewModel
+    {
+        public ShoppingCartService Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
