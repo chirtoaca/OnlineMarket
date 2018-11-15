529851adb3754d62d26ec8db667da6cdcde32a02
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-15 12:52:25 +0200

    Created Binders for cart, where save cart when browser is open

---
+++ OnlineMarket/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,37 @@
+﻿using OnlineMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace OnlineMarket.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            //get the Cart from the session
+            ShoppingCartService cart = null;
+            if (controllerContext.HttpContext.Session != null)
+            {
+                cart = (ShoppingCartService)controllerContext.HttpContext.Session[sessionKey];
+            }
+
+            //create the Cart if there wasn't one in the session data
+            if (cart == null)
+            {
+                cart = new ShoppingCartService();
+                if (controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+            //return the cart
+            return cart;
+        }
+    }
+}
