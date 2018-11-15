57fbb68530c7d77b15975c6fc5711c0ee7e9181c
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-15 12:48:55 +0200

    Implement IOrderProduct interface and defined methods in Service

---
+++ OnlineMarket.Core/Interfaces/IOrderProductService.cs
@@ -0,0 +1,20 @@
+﻿using OnlineMarket.DataModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Core.Interfaces
+{
+    public interface IOrderProductService
+    {
+        List<OrderProduct> OrdersProducts();
+
+        void AddOrderProducts(OrderProduct orderProducts);
+        void RemoveOrderProducts(OrderProduct orderProducts);
+        void EditOrderProducts(OrderProduct orderProducts);
+
+        OrderProduct GetOrderProducts(int id);
+    }
+}
---
+++ OnlineMarket.Core/Services/OrderService.cs
@@ -0,0 +1,48 @@
+﻿using OnlineMarket.Core.Interfaces;
+using OnlineMarket.DataModels.Models;
+using OnlineMarket.DataModels.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Core.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IRepository _repository;
+
+        public OrderService(IRepository repository)
+        {
+            _repository = repository;
+        }
+        public Order AddOrder(Order order)
+        {
+            var addedOrder = _repository.Add(order);
+            return addedOrder;
+        }
+
+        public void EditOrder(Order order)
+        {
+            _repository.Update(order);
+        }
+
+        public Order GetOrder(int id)
+        {
+            var result = _repository.GetAll<Order>().FirstOrDefault(o => o.Id == id);
+            return result;
+        }
+
+        public List<Order> GetOrders()
+        {
+            var result = _repository.GetAll<Order>().ToList();
+            return result;
+        }
+
+        public void RemoveOrder(Order order)
+        {
+            _repository.Delete(order);
+        }
+    }
+}
