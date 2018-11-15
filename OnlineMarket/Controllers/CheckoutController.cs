54f23d499365004983460198247d1b5a2d63a7d5
Gheorghe <gheorghe.chirt@yahoo.com>
2018-11-15 12:50:33 +0200

    created new controller Checkout for check order and write customer details

---
+++ OnlineMarket/Controllers/CheckoutController.cs
@@ -0,0 +1,50 @@
+﻿using OnlineMarket.Core.Interfaces;
+using OnlineMarket.DataModels.Models;
+using OnlineMarket.Entities;
+using OnlineMarket.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OnlineMarket.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public CheckoutController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+       
+        [HttpGet]
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+
+            var customerVM = new CustomerOrderViewModel
+            {
+                Id = customer.Id,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                District = customer.District,
+                City = customer.City,
+                Adress = customer.Adress,
+                PhoneNumber = customer.PhoneNumber,
+                Zip = customer.Zip
+            };
+            
+            //TempData["customerVM"] = customerVM;
+            return RedirectToAction("AddOrder", "Order", new RouteValueDictionary(customerVM));
+        }
+    }
+}
